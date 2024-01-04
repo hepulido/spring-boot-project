9a64b54ab6e891767be9bf901036a3768b7bcab9
hector <hpulido1021@gmail.com>
2024-01-04 13:41:25 -0500

    first commit

--- src/main/java/com/example/connectionApi/user/SecurityConfig.java
+++ src/main/java/com/example/connectionApi/user/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration @EnableWebSecurity public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.addFilter()
+        http.addFilter
                 .authorizeHttpRequests(authorizeRequest ->
                         authorizeRequest
                                 .requestMatchers("/api/v1/users/signIn")
